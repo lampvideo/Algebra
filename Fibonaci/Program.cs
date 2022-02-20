e4babebcde00603ceec871097c2a2f303cc053b5
lampvideo <alvk.klimov@gmail.com>
2022-02-20 19:31:52 +0300

    03 lesson

---
+++ Fibonaci/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fibonaci
+{
+    class Program
+    {
+        static int FibonaciR(int N) {
+            if (N == 0) return 0;
+            if (N == 1) return 1;
+            return FibonaciR( N - 1 ) + FibonaciR( N - 2 );
+
+        
+        }
+        static int FibonaciI(int N) {
+            int fib0 = 0;
+            int fib1 = 1;
+            if (N == 0) return fib0;
+            if (N == 1) return fib1;
+            int counter = 1;
+            int first, last;
+
+            first = fib0;
+            last = fib1;
+
+            
+            while (counter < N) {
+                int tmp = last;
+                last = first + last;
+                first = tmp;
+                counter++;
+            }
+            return last;
+
+        
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                int fib = FibonaciR(i);
+                Console.WriteLine($"i: {i} {fib}");
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int fibi = FibonaciI(i);
+                Console.WriteLine($"i: {i} {fibi}");
+            }
+        }
+    }
+}
---
+++ Prime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static bool isPrime(int k) {
+            bool flIsPrime = true;
+            for (int i = 2; i < k; i++)
+            {
+
+           
+                if ((i != k) && (k % i) == 0)
+                {
+                    flIsPrime = false;
+                    return flIsPrime;
+                }
+            }
+            return flIsPrime;
+        }
+        
+        static int findPrimes(int N) {
+            int num = 0;
+            for (int i = 2; i < N ; i++) {
+              
+                if ( isPrime(i) )
+                {
+                    Console.WriteLine($"Prime: {i}");                
+                    num++;
+                }
+                else {
+                  
+                }
+               
+                
+            }
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            int N = 12;
+            int num_primes = findPrimes(N);
+            Console.WriteLine($"Num primes less than {N}");
+            if (findPrimes(20) != 8) {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
